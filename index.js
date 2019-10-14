de14d1cc1d2a491b10f08cf0a017b03cdad8e4a3
yukiyao119 <ukiyao@hotmail.com>
2019-10-14 11:30:35 -0400

    fixed bugs

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
+  
+  // console.log(username)
+  // debugger
   let joke;
-
-  function fetchJoke(){
+  
+  function fetchJoke(username){
     fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => {
+      joke = jokeData.joke
+      const newJokeLi = document.createElement('li')
+      newJokeLi.innerHTML = `
+      <span class="username">${username} says:</span> ${joke}
+      `
+      jokeList.appendChild(newJokeLi)
+    })
   }
 
   form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let username = document.getElementById('name-input').value
 
     if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+    fetchJoke(username)
+
   })
 })
